6eec499eb1fcb632b64b8663cd5cce472524b849
Jamie Cockburn <jamiec@extramsp.com>
2018-11-22 14:29:08 +0000

    re #2 - Updated setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,27 @@
-from distutils.core import setup
+import setuptools
 
-setup(
+setuptools.setup(
     name='PyPTables',
-    version='1.0.3',
+    version='1.0.4',
     author='Jamie Cockburn',
     author_email='jamie_cockburn@hotmail.co.uk',
-    packages=['pyptables',
-              'pyptables.test',
-              'pyptables.rules',
-              'pyptables.rules.input',
-              'pyptables.rules.forwarding',
-              ],
-    scripts=[],
+    packages=setuptools.find_packages(),
     url='https://github.com/daggaz/python-pyptables',
     license='LICENSE.txt',
     description='Python package for generating Linux iptables configurations.',
     long_description=open('README.rst').read(),
-    requires=[
-        'six'
-    ],
+    keywords='iptables, firewall',
     install_requires=[
+        "six",
         "nose",
         "coverage",
+    ],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Topic :: System :: Networking :: Firewalls",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Operating System :: POSIX :: Linux",
     ],
 )
